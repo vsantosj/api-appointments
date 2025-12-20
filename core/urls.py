66904e636c61f0ad1866b225f9cd5481f0a4da3c
viviane santos <vjsantos.viviane@gmail.com>
2025-12-20 18:18:47 -0300

    chore: implements api versioning

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from health_professionals.views import HealthProfessionalCreateView, HealthProfessionalRetrieveUpdateDestroyView
-from appointments.views import AppointmentsCreateView, AppointmentsRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/professionals/', HealthProfessionalCreateView.as_view(), name='professionals-create-list'),
-    path('api/professionals/<int:pk>',HealthProfessionalRetrieveUpdateDestroyView.as_view(), name='professionals-detail-view'),
-
-    path('api/appointments/', AppointmentsCreateView.as_view(), name= 'appointments-create-list'),
-    path('api/appointments/<int:pk>', AppointmentsRetrieveUpdateDestroyAPIView.as_view(), name='appointments-detail-view'),
-
+    path('api/v1/', include('appointments.urls')),
+    path('api/v1/', include('health_professionals.urls')),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
---
+++ health_professionals/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns =[
+    path('professionals/', views.HealthProfessionalCreateView.as_view(), name='professionals-create-list'),
+    path('professionals/<int:pk>',views.HealthProfessionalRetrieveUpdateDestroyView.as_view(), name='professionals-detail-view'),
+]
